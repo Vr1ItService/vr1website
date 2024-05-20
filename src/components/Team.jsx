1e5242486ee6cf5d7624096b07b8c02660c8146f
Vr1ItService <167622256+Vr1ItService@users.noreply.github.com>
2024-05-20 12:11:24 +0530

    UpdateTeam.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -71,7 +71,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>SRIRAM THOTA</h4>
-                                    <h6 className="mb-0">Founder & Director</h6>
+                                    <h5 className="mb-0">Founder & Director</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -103,7 +103,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>B SRINIVAS RAO</h4>
-                                    <h6 className="mb-0">Co-Director</h6>
+                                    <h5 className="mb-0">Co-Director</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -133,7 +133,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>VICTOR GERALD</h4>
-                                    <h6 className="mb-0">Principal Advisor</h6>
+                                    <h5 className="mb-0">Principal Advisor</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -163,7 +163,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4 style={{fontSize:'23px'}}>VENNELA DASARI</h4>
-                                    <h6 className="mb-0">HR Manager</h6>
+                                    <h5 className="mb-0">HR Manager</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -203,7 +203,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>MATHEW HENRY CHODISEETY</h4>
-                                    <h6 className="mb-0">IT Manager</h6>
+                                    <h5 className="mb-0">IT Manager</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -235,7 +235,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>NAGA PRASAD CHINTAPALLI</h4>
-                                    <h6 className="mb-0">Project Manager</h6>
+                                    <h5 className="mb-0">Project Manager</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -265,7 +265,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>B SHIVA NAGA RAJU</h4>
-                                    <h6 className="mb-0">Admin Manager</h6>
+                                    <h5 className="mb-0">Admin Manager</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -297,7 +297,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>YASHASWINI NAVILE Swamy</h4>
-                                    <h6 className="mb-0">Accounts Manager</h6>
+                                    <h5 className="mb-0">Accounts Manager</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
@@ -327,7 +327,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>SONALI <br />TORE</h4>
-                                    <h6 className="mb-0">TEAM LEAD</h6>
+                                    <h5 className="mb-0">TEAM LEAD</h5>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
