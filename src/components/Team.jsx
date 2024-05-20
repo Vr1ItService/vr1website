093a202ed69a9de71a5186b28dd9606c6152bbcd
Vr1ItService <167622256+Vr1ItService@users.noreply.github.com>
2024-05-20 15:34:44 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -103,7 +103,7 @@ function Team() {
                             <div className="team_h1mo position-relative clearfix">
                                 <div className="team_h1mo1 text-white clearfix">
                                     <h4>B SRINIVAS RAO</h4>
-                                    <h6 className="mb-0"style={{fontSize:'22px'}}>Co-Director</h6>
+                                    <h6 className="mb-0" style={{fontSize:'22px'}}>Co-Director</h6>
                                 </div>
                                 <div className="team_h1mo2 position-absolute clearfix">
                                     <a href="#"><span><FaShareAlt /></span></a>
